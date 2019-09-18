87d6042b2e60caa75bbe1b366f0cb35cc21770a1
OscarVanL <7999692+OscarVanL@users.noreply.github.com>
2019-09-18 11:33:34 +0100

    Created empty UI and open it

--- clustering_gui.py
+++ clustering_gui.py
@@ -1,13 +1,37 @@
-import k_means
+from k_means import KMeans
 import numpy as np
 import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5.QtCore import pyqtSlot
+from PyQt5 import uic
+import os
+import sys
 
-def main():
-    pass
+main_interface_file = os.path.join('layout', 'main_interface.ui')  # OS-safe path slashes
 
-main()
-
-class ClusteringGui:
+class ClusteringGui(QMainWindow):
+    kmeans = None
 
     def __init__(self):
-        pass
+        self.kmeans = KMeans(dimensions=2)
+        super(ClusteringGui, self).__init__()
+        uic.loadUi(main_interface_file, self)
+        self.browse = self.findChild(QPushButton, "browse_button")
+        self.browse.clicked.connect(self.on_browse_click)
+
+        self.show()
+
+    @pyqtSlot()
+    def on_browse_click(self):
+        print("Button clicked")
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = ClusteringGui()
+    window.show()
+    sys.exit(app.exec_())
+    pass
+
+
+main()
--- k_means.py
+++ k_means.py
@@ -1,6 +1,7 @@
 
 
 class KMeans:
+    dimensions = None  # Number of dimensions to K-Means Vectors
 
-    def __init__(self):
-        pass
+    def __init__(self, dimensions: int):
+        self.dimensions = dimensions
